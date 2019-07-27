1546cce1b22998f5d66a42367f49448248b4024f
Marcal Ramos <suncastelo@gmail.com>
2019-07-27 05:15:44 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+let playlist = {
+  MichaelJackson: 'triller'
+}
+
+function updatePlaylist(playlist, name, song){
+  
+  return playlist
+}
+
+function updatePlaylist(playlist, name, song){
+  playlist([MichaelJackson] = 'Samba');
+  return playlist
+}
